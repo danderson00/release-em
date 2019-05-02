dabb82f575d3f85058cef060f6c88355a3f2f891
Dale Anderson <danderson00@gmail.com>
2019-05-02 20:38:14 +1000

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const release = require('release-it')
 
-// require('release-it/lib/plugin/version/Version').isEnabled = () => false
+require('release-it/lib/plugin/version/Version').isEnabled = () => false
 
 release({})
