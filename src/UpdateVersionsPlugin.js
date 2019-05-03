a03c3d9880e589d951c322632a97c7bdce4f3b46
Dale Anderson <danderson00@gmail.com>
2019-05-03 23:43:29 +1000

    Add workaround. Fix dry run

--- src/UpdateVersionsPlugin.js
+++ src/UpdateVersionsPlugin.js
@@ -3,11 +3,19 @@ const { Plugin } = require('release-it')
 const fs = require('fs')
 
 module.exports = class UpdateVersionsPlugin extends Plugin {
+  // if the npm release-it plugin is disabled, the name and versions are not populated
+  // this is a bit of a workaround, but the versions it reports are incorrect (it's not upgrading at all...)
+  getName() {
+    return JSON.parse(fs.readFileSync('package.json')).name
+  }
+
   bump() {
     if(!this.options.updatedDependencies) {
       throw new Error("No dependency information was provided. The UpdateVersionsPlugin plugin is intended to be run using the release-em CLI tool.")
     }
     
+    this.log.exec('Updating dependencies', this.global.isDryRun)
+
     if(!this.global.isDryRun) {
       const packageJson = JSON.parse(fs.readFileSync('package.json')) // cwd is set by release-em
       const updatedPackageJson = updatePackageVersions(packageJson, this.options.updatedDependencies)
