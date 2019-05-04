df2b2a3ade562c7740bdbff7f0817e21be40625c
Dale Anderson <danderson00@gmail.com>
2019-05-05 00:51:08 +1000

    Clean up

--- src/CheckUpstreamPlugin.js
+++ src/CheckUpstreamPlugin.js
@@ -1,16 +1,16 @@
 const { Plugin } = require('release-it')
 
-const options = { write: false }
+const readOnlyOperation = { options: { write: false } }
 
 module.exports = class CheckUpstreamPlugin extends Plugin {
   async beforeBump() {
     if(!!this.config.options.git) {
-      await this.exec('git remote update', { options })
-      const remote = await this.exec('git remote show', { options })
+      await this.exec('git remote update', readOnlyOperation)
+      const remote = await this.exec('git remote show', readOnlyOperation)
 
       const upstreamCommits = Number(await this.exec(
         `git rev-list HEAD..${remote}/HEAD --count`, 
-        { options }
+        readOnlyOperation
       ))
 
       if(upstreamCommits === NaN) {
